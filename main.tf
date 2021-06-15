a1194dc9bafcc0f9a44b2b9d3c87f1661c76aceb
Leandro Basilio <leandro.basilio@cortex-intelligence.com>
2021-06-15 11:26:26 -0300

    alterando ami e tipo de instancia

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ terraform {
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 
@@ -30,8 +30,8 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
+  ami                    = "ami-0aeeebd8d2ab47354"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
